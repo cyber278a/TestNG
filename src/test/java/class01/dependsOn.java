95e4a05efc8c2233fb9c1fef862a482fa344a5cd
cyber278a <cyber278a@gmail.com>
2023-04-12 16:47:44 -0400

    V1

---
+++ src/test/java/class01/dependsOn.java
@@ -0,0 +1,16 @@
+package class01;
+
+import org.testng.annotations.Test;
+
+public class dependsOn {
+    @Test
+    public void  Login(){
+       // System.out.println("i am here");
+         System.out.println(6/0);
+    }
+
+    @Test(dependsOnMethods = {"Login"} )
+    public  void DashBoardverification(){
+        System.out.println("after login i am verifying dashboard");
+    }
+}
---
+++ src/test/java/class01/priority.java
@@ -0,0 +1,26 @@
+package class01;
+
+import org.testng.annotations.Test;
+
+public class priority {
+
+// executing inpriority  order(or in alphabetical-"aTest,bTest,ext if priority not there)
+
+    @Test(priority = 3)
+    public void Atest(){
+        System.out.println("i am TestA");
+    }
+
+
+    @Test(priority = 1)
+    public void Btest(){
+        System.out.println("i am TestB");
+    }
+
+
+    @Test(priority = 2)
+    public void Ctest(){
+        System.out.println("i am TestC");
+    }
+
+}
